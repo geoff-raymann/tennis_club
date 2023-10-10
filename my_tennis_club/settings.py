ef56aaeb5bcbe130250e8422159650228c0df1f5
Geoffray <geoffray@localhost.localdomain>
2023-10-10 18:13:56 +0300

    Correct code

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cft^h%$m+2mv9%0%pf5m+r1cge4sskh+k4tq8a)kfkz*tbf&bf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
